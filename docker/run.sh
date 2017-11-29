01c2e4e9ffdf1948084256404337b3b1e0191ceb
SBI- <pascal.huerlimann@gmail.com>
2017-11-29 10:53:57 +0100

    Extract to container directory

--- docker/run.sh
+++ docker/run.sh
@@ -3,18 +3,18 @@
 # this should always find exactly one jar
 equinoxLauncher=$(find / -regextype posix-extended -regex ".*org.eclipse.equinox.launcher_[0-9.v-]+.dist.jar" 2> /dev/null)
 
-mkdir /output
+mkdir /deflated
 
 for zip in /sdks/*.zip; do
     base=${zip##*/}
     fname=${base%.*}
     
-    unzip $zip -d /sdks/$fname
+    unzip $zip -d /deflated/$fname
 
     java -jar $equinoxLauncher \
         -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
-        -metadataRepository file:/output/$fname/ \
-        -artifactRepository file:/output/$fname/ \
+        -metadataRepository file:/deflated/$fname/ \
+        -artifactRepository file:/deflated/$fname/ \
         -source /sdks/$fname \
         -publishArtifacts \
         -compress \;
